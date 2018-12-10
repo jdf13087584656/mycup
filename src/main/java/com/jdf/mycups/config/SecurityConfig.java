c556bc81c2b1defb0f9bc05be5cd8720e1d9a285
jindingfeng <13087584656@163.com>
2018-12-10 17:34:11 +0800

    用户认证已完成

--- src/main/java/com/jdf/mycups/config/SecurityConfig.java
+++ src/main/java/com/jdf/mycups/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
     }
     @Override
     public void configure(HttpSecurity http)throws Exception{
-        http.formLogin().loginPage("/login").loginProcessingUrl("/login/form").failureUrl("/login-error").permitAll()  //表单登录，permitAll()表示这个不需要验证 登录页面，登录失败页面
+        http.formLogin().loginPage("/login").loginProcessingUrl("/login/form").defaultSuccessUrl("/index").failureUrl("/login-error").permitAll()  //表单登录，permitAll()表示这个不需要验证 登录页面，登录失败页面
                 .and()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
